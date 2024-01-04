1f58af58e6eb80aac8183e1bfcf6095b1b0aa130
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2024-01-04 13:26:28 +0300

    Added resume component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Footer from './components/Footer';
-// import Resume from './components/Resume/ResumeNew';
+import Resume from './components/Resume/ResumeNew';
 import ScrollToTop from './components/ScrollToTop';
 import './style.css';
 import './App.css';
@@ -38,7 +38,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/resume" element={<Resume />} /> */}
+          <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
